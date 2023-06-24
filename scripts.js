bd0ae96dfcebcbbf9bdfb3111033f07d8115d52c
blajd <blajd@LAPTOP-MO3R6A8A>
2023-06-25 01:57:06 +0300

    Add project files.

---
+++ scripts.js
@@ -0,0 +1,28 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var scrollButton = document.querySelector('.scroll-button');
+
+    scrollButton.addEventListener('click', function () {
+        if (window.scrollY < window.innerHeight / 2) {
+            window.scrollTo({
+                top: document.body.scrollHeight,
+                behavior: 'smooth'
+            });
+        } else {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            });
+        }
+    });
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY < window.innerHeight / 2) {
+            scrollButton.classList.remove('up');
+        } else {
+            scrollButton.classList.add('up');
+        }
+    });
+});
+
+
+
